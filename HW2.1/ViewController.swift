a54bdded92113b8c31b714e4bc99a6fb4c26ad16
MaximShvanov <shvanov.m@yandex.ru>
2020-08-19 22:41:19 +0300

    Initial Commit

---
+++ HW2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.1
+//
+//  Created by Maxon on 19.08.2020.
+//  Copyright © 2020 Maxim Shvanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
